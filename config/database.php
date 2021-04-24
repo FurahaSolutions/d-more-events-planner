ecc94d6ac2e937eceb42f3e86e67173aaaaaaf3b
OwenFurahasolutions <owen@furahasolutions.tech>
2021-04-24 19:33:39 +0300

    restarted config

--- config/database.php
+++ config/database.php
@@ -106,9 +106,9 @@ return [
         'testing' => [
             'driver' => 'mysql',
             'host' => env('DB_TEST_HOST', 'localhost'),
-            'database' => env('DB_TEST_DATABASE', 'dmore_testing'),
+            'database' => env('DB_TEST_DATABASE', 'furahasms_testing'),
             'username' => env('DB_TEST_USERNAME', 'root'),
-            'password' => env('DB_TEST_PASSWORD', ''),
+            'password' => env('DB_TEST_PASSWORD', 'secret'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
