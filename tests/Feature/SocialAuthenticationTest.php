475a0c748811ce8f7c1b32110ee6258b247e2503
OwenFurahasolutions <owen@furahasolutions.tech>
2021-04-28 11:28:36 +0300

    Added tests for social login

---
+++ tests/Feature/SocialAuthenticationTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Laravel\Socialite\Facades\Socialite;
+use Mockery;
+use Tests\TestCase;
+
+class SocialAuthenticationTest extends TestCase
+{
+    /**
+     * A basic feature test example
+     * @group auth
+     * @test
+     * @return void
+     */
+    public function test_user_can_login_using_google_account()
+    {
+        $providerName = 'google';
+        $socialAccount = User::factory()->create(['provider' => $providerName]);
+        $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+        $abstractUser->shouldReceive('getId')
+            ->andReturn($socialAccount->provider_user_id)
+            ->shouldReceive('getEmail')
+            ->andReturn($this->faker->email)
+            ->shouldReceive('getName')
+            ->andReturn($this->faker->name)
+            ->shouldReceive('getAvatar')
+            ->andReturn('https://en.gravatar.com/userimage');
+
+        $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+        $provider->shouldReceive('user')->andReturn($abstractUser);
+        $provider->shouldReceive('stateless->user')
+            ->andReturn($abstractUser);
+
+        Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
+        $this->get('/auth/callback/google')
+            ->assertStatus(302);
+    }
+
+    /**
+     * A basic feature test example
+     * @group auth
+     * @test
+     * @return void
+     */
+    public function test_user_can_login_using_facebook_account()
+    {
+        $providerName = 'google';
+        $socialAccount = User::factory()->create(['provider' => $providerName]);
+        $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+        $abstractUser->shouldReceive('getId')
+            ->andReturn($socialAccount->provider_user_id)
+            ->shouldReceive('getEmail')
+            ->andReturn($this->faker->email)
+            ->shouldReceive('getName')
+            ->andReturn($this->faker->name)
+            ->shouldReceive('getAvatar')
+            ->andReturn('https://en.gravatar.com/userimage');
+
+        $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+        $provider->shouldReceive('user')->andReturn($abstractUser);
+        $provider->shouldReceive('stateless->user')
+            ->andReturn($abstractUser);
+
+        Socialite::shouldReceive('driver')->with('facebook')->andReturn($provider);
+        $this->get('/auth/callback/facebook')
+            ->assertStatus(302);
+    }
+
+    /**
+     * A basic feature test example
+     * @group auth
+     * @test
+     * @return void
+     */
+    public function test_user_can_login_using_twitter_account()
+    {
+        $providerName = 'google';
+        $socialAccount = User::factory()->create(['provider' => $providerName]);
+        $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+        $abstractUser->shouldReceive('getId')
+            ->andReturn($socialAccount->provider_user_id)
+            ->shouldReceive('getEmail')
+            ->andReturn($this->faker->email)
+            ->shouldReceive('getName')
+            ->andReturn($this->faker->name)
+            ->shouldReceive('getAvatar')
+            ->andReturn('https://en.gravatar.com/userimage');
+
+        $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+        $provider->shouldReceive('user')->andReturn($abstractUser);
+        $provider->shouldReceive('stateless->user')
+            ->andReturn($abstractUser);
+
+        Socialite::shouldReceive('driver')->with('twitter')->andReturn($provider);
+        $this->get('/auth/callback/twitter')
+            ->assertStatus(302);
+    }
+}
